b585916dce872c8203d1d988c835ed6ff844bb43
KennethUlloa <ulloakenth@gmail.com>
2023-04-09 20:43:05 -0500

    Nav bar, toggle button

---
+++ dist/js/ln-autoload.js
@@ -0,0 +1,16 @@
+function prepareToggleButtons() {
+    let btns = document.querySelectorAll('.ln-nav-toggle-btn');
+    btns.forEach(btn => {
+        if (btn.hasAttribute("data-ln-target")) {
+            btn.addEventListener('click', () => {
+                let target = document.querySelector(btn.getAttribute("data-ln-target"));
+                if (target !== null) {
+                    target.classList.toggle('ln-initial');
+                }
+            });
+        }
+    });
+}
+window.onload = () => {
+    prepareToggleButtons();
+};
